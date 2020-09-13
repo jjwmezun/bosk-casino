5f216aa28f298a4f9e83f013a5b9a788c5578ebe
J. J. W. Mezun <jjwmezun@gmail.com>
2020-09-13 01:40:31 -0700

    Clean up script code

--- src/script.ts
+++ src/script.ts
@@ -43,6 +43,9 @@ import { Text } from './text';
 			let paragraphs:string[] = this.addParagraphs( this.generateIntroText(), this.firstRollText( data ) );
 			for ( const turn of data.turnList )
 			{
+				if ( turn.number === 11 ) {
+					paragraphs = this.addParagraphs( paragraphs, this.dinnerText( data ) );
+				}
 				paragraphs = this.addParagraphs( paragraphs, this.getRollText( data, turn ) );
 				for ( const pass of turn.passes )
 				{
@@ -63,23 +66,23 @@ import { Text } from './text';
 			}
 			return Object.freeze( newList );
 		},
-		addParagraphs: function( list:string[], newParagraphs:string[] ):string[]
+		addParagraphs: function( list:string[], newParagraphs:string[] ):readonly string[]
 		{
 			let newList:string[] = list.slice( 0 );
 			for ( const string of newParagraphs )
 			{
 				newList = this.addParagraph( newList, string );
 			}
-			return newList;
+			return Object.freeze( newList );
 		},
-		addParagraphLists: function( listOfLists ):string[]
+		addParagraphLists: function( listOfLists ):readonly string[]
 		{
 			let paragraphs:string[] = [];
 			for ( const list of listOfLists )
 			{
 				paragraphs = this.addParagraphs( paragraphs, list );
 			}
-			return paragraphs;
+			return Object.freeze( paragraphs );
 		},
 		generateIntroText: function():readonly string[]
 		{
@@ -1308,7 +1311,7 @@ import { Text } from './text';
 								`<If you insist>. Dawn began rolling the die with frantic energy, & then threw the die on the ground with aplomb. She looked down & saw that she’d rolled a ${ turn.roll }.`
 							]
 						});
-						return paragraphs.concat( choices[ config.players[ analyze.getTurnPlayer( game, turn ) ] ] );
+						return this.addParagraphs( paragraphs, choices[ config.players[ analyze.getTurnPlayer( game, turn ) ] ] );
 					}
 					break;
 
@@ -1448,41 +1451,48 @@ import { Text } from './text';
 				]
 			);
 		},
+		dinnerText: function( game:Game ):readonly string[]
+		{
+			return [
+				``
+			];
+		},
 		firstRollText: function( game:Game ):readonly string[]
 		{
 			const roll:number = game.turnList[ 1 ].roll;
-			let paragraphs = [ `<OK, ¿now who should roll 1st?>, asked Dawn.` ];
-
-			const choices =
-			{
-				Autumn:
+			return this.addParagraphs(
+				this.addParagraphs(
+					[ `<OK, ¿now who should roll 1st?>, asked Dawn.` ],
+					{
+						Autumn:
+						[
+							`Autumn shrugged. <You’re the one experienced in this game, so you would know best, save for maybe the roll o’ a die>.`,
+							`Dawn tilted her head in feigned thought. <Well, since Autumn is such a smarty sweats, I choose her to go 1st>. Dawn held out the die toward Autumn.`,
+							`<Whatever tickles your bones>, Autumn said as she took the die from Dawn.`,
+							`Autumn rolled the die for less than a second in 1 limp wrist before flicking it out onto the floor. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
+						],
+						Edgar:
+						[
+							`Autumn shrugged. <You’re the one experienced in this game, so you would know best, save for maybe the roll o’ a die>.`,
+							`<OK, let me think…>. Dawn rubbed her mouth. Then she turned to Edgar & put a hand on his shoulder. <Edgar, ¿why don’t you lead for once?>.`,
+							`<Uh, ¿are you sure?>, Edgar asked, wringing his hands together.`,
+							`<’Course I’m sure>.`,
+							`<Well, OK>. Edgar took the die from Dawn & began rolling it. He took a deep breath, & then finally released the die to the ground as if pushing it ’way. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
+						],
+						Dawn:
+						[
+							`<Since you’re the one most experienced in this game, you would be the most logical choice>, said Autumn.`,
+							`<If you insist>. Dawn began rolling the die with frantic energy, & then threw the die on the ground with aplomb. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
+						]
+					}[ config.players[ game.playerOrder[ 0 ] ] ]
+				),
 				[
-					`Autumn shrugged. <You’re the one experienced in this game, so you would know best, save for maybe the roll o’ a die>.`,
-					`Dawn tilted her head in feigned thought. <Well, since Autumn is such a smarty sweats, I choose her to go 1st>. Dawn held out the die toward Autumn.`,
-					`<Whatever tickles your bones>, Autumn said as she took the die from Dawn.`,
-					`Autumn rolled the die for less than a second in 1 limp wrist before flicking it out onto the floor. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
-				],
-				Edgar:
-				[
-					`Autumn shrugged. <You’re the one experienced in this game, so you would know best, save for maybe the roll o’ a die>.`,
-					`<OK, let me think…>. Dawn rubbed her mouth. Then she turned to Edgar & put a hand on his shoulder. <Edgar, ¿why don’t you lead for once?>.`,
-					`<Uh, ¿are you sure?>, Edgar asked, wringing his hands together.`,
-					`<’Course I’m sure>.`,
-					`<Well, OK>. Edgar took the die from Dawn & began rolling it. He took a deep breath, & then finally released the die to the ground as if pushing it ’way. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
-				],
-				Dawn:
-				[
-					`<Since you’re the one most experienced in this game, you would be the most logical choice>, said Autumn.`,
-					`<If you insist>. Dawn began rolling the die with frantic energy, & then threw the die on the ground with aplomb. They all looked down @ it, Edgar & Dawn bending close to it to get a closer look while Autumn only budged her pupils.`
+					`The die said “${ roll }”.`,
+					`<That means we walk down ${ roll } o’ these hexagon spaces>, Dawn said, already beginning to move. She looked back @ Autumn & Edgar. <Make sure you [i]only[/i] walk that many steps, & don’t stop before or after. The cranes that run this place are anal ’bout the rules>.`,
+					`Autumn frowned. <Cranes. Great. I always wanted to die in frivolity>.`,
+					`<It’ll be all right. C’mon>. Dawn waved her fingers toward her, beckoning them. They followed.`
 				]
-			};
-			paragraphs = paragraphs.concat( choices[ config.players[ game.playerOrder[ 0 ] ] ] );
-			paragraphs.push( `The die said “${ roll }”.` );
-			paragraphs.push( `<That means we walk down ${ roll } o’ these hexagon spaces>, Dawn said, already beginning to move. She looked back @ Autumn & Edgar. <Make sure you [i]only[/i] walk that many steps, & don’t stop before or after. The cranes that run this place are anal ’bout the rules>.` );
-			paragraphs.push( `Autumn frowned. <Cranes. Great. I always wanted to die in frivolity>.` );
-			paragraphs.push( `<It’ll be all right. C’mon>. Dawn waved her fingers toward her, beckoning them. They followed.` );
-
-			return Object.freeze( paragraphs );
+			);
 		},
 		playerNameText: function( player:number ):string
 		{
